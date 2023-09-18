7889d9dad31d0a96e7bbfa1497f28b3eee161d88
thegamer256 <lanhtuan65@gmail.com>
2023-09-19 00:04:30 +0700

    feat[SP-010]: Add delete Address

--- routes/address.js
+++ routes/address.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const { createAddress, getAddress } = require('../controllers/address')
+const { createAddress, getAddress, deleteAddress } = require('../controllers/address')
 const router = express.Router()
 
 router.route('/').post(createAddress)
 router.route('/:userId').get(getAddress)
+router.route('/:userId/:addressId').delete(deleteAddress)
 
 module.exports = router
