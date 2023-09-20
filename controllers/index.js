e1193b2762e75001e09077f3694fcc227e12b05f
TungPhan2808 <phanmanhtung123@gmail.com>
2023-09-20 21:57:10 +0700

    feat: Shop request, get all requests of a user

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,12 @@
 const { register, login } = require('./auth')
 const { createAddress, getAddress } = require('./address')
+const { getAllRequestByUserId, shopRegister } = require('./shopRequest')
 
 module.exports = {
     register,
     login,
     createAddress,
     getAddress,
+    getAllRequestByUserId,
+    shopRegister
 }
--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const morgan = require('morgan')
 // import middlewares
 const errorHandler = require('./middlewares/errorHandler')
 const notFound = require('./middlewares/notFound')
-const { authRouter, addressRouter } = require('./routes')
+const { authRouter, addressRouter, shopRequestRouter } = require('./routes')
 
 const app = express()
 const PORT = process.env.PORT || 5001
@@ -21,6 +21,7 @@ app.use(express.json())
 // routes
 app.use('/auth', authRouter)
 app.use('/address', addressRouter)
+app.use('/shop-request', shopRequestRouter)
 
 // error handler
 app.use(notFound)
