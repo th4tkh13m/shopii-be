9e02ee050d59fa6c2a857381087aaa71782ddf06
TungPhan2808 <phanmanhtung123@gmail.com>
2023-10-07 22:37:03 +0700

    refactor[SP-009]: throw error and update customer

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,15 +1,17 @@
 const { StatusCodes } = require('http-status-codes')
-const { ShopRequest, Shop } = require('../models/index')
+const { ShopRequest, Shop, Customer } = require('../models/index')
 const { createCustomError } = require('../errors/CustomError')
 
 const getRequestByStatus = async (req, res) => {
-    const request = await ShopRequest.find({ status: req.query.status })
-    if (request.length == 0) {
-        const customError = createCustomError(
-            'Không có yêu cầu nào.',
-            StatusCodes.NOT_FOUND,
-        )
-        throw customError
+    const status = req.query.status
+    let request = null
+    if (status === 'All') {
+        request = await ShopRequest.find()
+    } else {
+        request = await ShopRequest.find({ status })
+    }
+    if (request.length === 0) {
+        throw createCustomError('Không có yêu cầu nào.', StatusCodes.NOT_FOUND)
     }
     res.status(StatusCodes.OK).json(request)
 }
@@ -37,9 +39,14 @@ const handleShopRequest = async (req, res) => {
             shopDescription: description,
             userId,
         })
+        const updateCustomer = await Customer.findByIdAndUpdate(
+            { _id: userId },
+            { hasShop: true },
+        )
         return res.status(StatusCodes.OK).json({
             msg: 'Đã chấp nhận.',
             shop,
+            updateCustomer,
         })
     }
     res.status(StatusCodes.OK).json({
