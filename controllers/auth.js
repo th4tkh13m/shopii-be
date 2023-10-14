87ef30cced80ee95c1b4fa6ca5a8b4549b478194
Khiem Ton <th4tkh13m@protonmail.com>
2023-10-14 19:48:59 +0700

    fix[SP-012]: change the quals

--- controllers/auth.js
+++ controllers/auth.js
@@ -115,14 +115,14 @@ const resetPassword = async (req, res) => {
     const _token = await Token.findById(tokenId).populate('customer').exec()
 
     console.log(_token)
-    if (_token.token == token) {
+    if (_token.token === token) {
         throw createCustomError(
             `Mã xác thục ${code} không đúng`,
             StatusCodes.BAD_REQUEST,
         )
     }
 
-    if (password != rePassword) {
+    if (password !== rePassword) {
         throw createCustomError('Mật khẩu không khớp.', StatusCodes.BAD_REQUEST)
     }
 
