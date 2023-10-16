411d509a007b84978d01378c2fc43d4d6d4f8c04
TungPhan2808 <phanmanhtung123@gmail.com>
2023-10-16 16:18:42 +0700

    fix[SP-026-dev]: rewrite router and updateShop

--- routes/shop.js
+++ routes/shop.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const { getAllShopInfo, updateShop } = require('../controllers')
-const { verifyShop, verifyUser } = require('../middlewares/authenticateToken')
 const router = express.Router()
 const {
     getAllProducts,
