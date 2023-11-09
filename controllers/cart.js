a3f7fbb9bd379a52f9a5caac0ff9425403db47e5
TungPhan2808 <phanmanhtung123@gmail.com>
2023-11-09 23:05:20 +0700

    fix[SP-027]: update Schema and controller

--- controllers/cart.js
+++ controllers/cart.js
@@ -6,17 +6,18 @@ const viewCartByUserId = async (req, res) => {
     const userId = req.user.userId
     const cart = await Cart.find({ userId })
         .populate({
-            path: 'products.product',
+            path: 'cart.products.product',
             select: 'id productName productCategory productImages',
             populate: {
                 path: 'productCategory',
-                select: 'id name',
+                select: 'id categoryName',
             },
         })
         .populate({
-            path: 'products.productOption',
-            select: 'id optionPrice',
+            path: 'cart.products.productOption',
+            select: 'id optionName optionPrice',
         })
+        .populate('cart.shopId', 'id')
         .exec()
 
     res.status(StatusCodes.OK).json(cart)
@@ -24,37 +25,54 @@ const viewCartByUserId = async (req, res) => {
 
 const addToCart = async (req, res) => {
     const userId = req.user.userId
-    const { productId, productOptionId, quantity } = req.body
-    const cart = await Cart.findOne({ userId })
+    const { shopId, productId, productOptionId, quantity } = req.body
+    const userCart = await Cart.findOne({ userId })
     const newProduct = {
         product: productId,
         productOption: productOptionId,
         quantity: quantity || 1,
     }
 
-    if (cart) {
-        const existingProductIndex = cart.products.findIndex(
-            item =>
-                item.product.equals(productId) &&
-                item.productOption.equals(productOptionId),
+    if (userCart) {
+        const existingShop = userCart.cart.find(item =>
+            item.shopId.equals(shopId),
         )
+        if (existingShop) {
+            const existingProductIndex = existingShop.products.findIndex(
+                product =>
+                    product.product?.equals(productId) &&
+                    product.productOption?.equals(productOptionId),
+            )
+            if (existingProductIndex !== -1) {
+                const existingProduct =
+                    existingShop.products[existingProductIndex]
 
-        if (existingProductIndex !== -1) {
-            if (quantity !== undefined) {
-                cart.products[existingProductIndex].quantity += quantity
+                if (quantity !== undefined) {
+                    existingProduct.quantity += quantity
+                } else {
+                    existingProduct.quantity += 1
+                }
             } else {
-                cart.products[existingProductIndex].quantity += 1
+                existingShop.products.push(newProduct)
             }
         } else {
-            cart.products.push(newProduct)
+            userCart.cart.push({
+                shopId,
+                products: [newProduct],
+            })
         }
 
-        await cart.save()
-        res.status(StatusCodes.OK).json(cart)
+        await userCart.save()
+        res.status(StatusCodes.OK).json(userCart)
     } else {
         const newCart = await Cart.create({
             userId,
-            products: [newProduct],
+            cart: [
+                {
+                    shopId,
+                    products: [newProduct],
+                },
+            ],
         })
         res.status(StatusCodes.OK).json(newCart)
     }
@@ -62,55 +80,66 @@ const addToCart = async (req, res) => {
 
 const updateCartProduct = async (req, res) => {
     const userId = req.user.userId
-    const { productId, productOptionId, quantity, newProductOptionId } =
+    const { shopId, productId, productOptionId, quantity, newProductOptionId } =
         req.body
-    const cart = await Cart.findOne({ userId })
+    const userCart = await Cart.findOne({ userId })
 
-    const existingProductIndex = cart.products.findIndex(
-        item =>
-            item.product.equals(productId) &&
-            item.productOption.equals(productOptionId),
-    )
-    if (existingProductIndex !== -1) {
-        if (quantity !== undefined) {
-            cart.products[existingProductIndex].quantity = quantity
-        }
+    const existingShop = userCart.cart.find(item => item.shopId.equals(shopId))
 
-        if (newProductOptionId) {
-            cart.products[existingProductIndex].productOption =
-                newProductOptionId
-        }
-
-        await cart.save()
-        res.status(StatusCodes.OK).json(cart)
-    } else {
-        throw createCustomError(
-            'Không tìm thấy sản phẩm',
-            StatusCodes.NOT_FOUND,
+    if (existingShop) {
+        const existingProductIndex = existingShop.products.findIndex(
+            item =>
+                item.product?.equals(productId) &&
+                item.productOption?.equals(productOptionId),
         )
+        if (existingProductIndex !== -1) {
+            if (quantity !== undefined) {
+                existingShop.products[existingProductIndex].quantity = quantity
+            }
+
+            if (newProductOptionId) {
+                existingShop.products[existingProductIndex].productOption =
+                    newProductOptionId
+            }
+
+            await userCart.save()
+            res.status(StatusCodes.OK).json(userCart)
+        } else {
+            throw createCustomError(
+                'Không tìm thấy sản phẩm',
+                StatusCodes.NOT_FOUND,
+            )
+        }
     }
+    throw createCustomError(
+        'Không tìm thấy sản phẩm trong giỏ hàng',
+        StatusCodes.NOT_FOUND,
+    )
 }
 
 const deleteCartProduct = async (req, res) => {
     const userId = req.user.userId
-    const { productId, productOptionId } = req.body
-    const cart = await Cart.findOne({ userId })
+    const { shopId, productId, productOptionId } = req.body
+    const userCart = await Cart.findOne({ userId })
 
-    const existingProductIndex = cart.products.findIndex(
-        item =>
-            item.product.equals(productId) &&
-            item.productOption.equals(productOptionId),
-    )
+    const existingShop = userCart.cart.find(item => item.shopId.equals(shopId))
 
-    if (existingProductIndex !== -1) {
-        cart.products.splice(existingProductIndex, 1)
-        await cart.save()
-        res.status(StatusCodes.OK).json(cart)
-    } else {
-        throw createCustomError(
-            'Không tìm thấy sản phẩm',
-            StatusCodes.NOT_FOUND,
+    if (existingShop) {
+        const existingProductIndex = existingShop.products.findIndex(
+            product =>
+                product.product.equals(productId) &&
+                product.productOption.equals(productOptionId),
         )
+        if (existingProductIndex !== -1) {
+            existingShop.products.splice(existingProductIndex, 1)
+            await userCart.save()
+            res.status(StatusCodes.OK).json(userCart)
+        } else {
+            throw createCustomError(
+                'Không tìm thấy sản phẩm',
+                StatusCodes.NOT_FOUND,
+            )
+        }
     }
 }
 
--- models/Cart.js
+++ models/Cart.js
@@ -5,19 +5,27 @@ const CartSchema = new mongoose.Schema({
         type: mongoose.Types.ObjectId,
         ref: 'Customer',
     },
-    products: [
+    cart: [
         {
-            product: {
+            shopId: {
                 type: mongoose.Types.ObjectId,
-                ref: 'Product',
+                ref: 'Shop',
             },
-            productOption: {
-                type: mongoose.Types.ObjectId,
-                ref: 'ProductOption',
-            },
-            quantity: {
-                type: Number,
-            },
+            products: [
+                {
+                    product: {
+                        type: mongoose.Types.ObjectId,
+                        ref: 'Product',
+                    },
+                    productOption: {
+                        type: mongoose.Types.ObjectId,
+                        ref: 'ProductOption',
+                    },
+                    quantity: {
+                        type: Number,
+                    },
+                },
+            ],
         },
     ],
 })
